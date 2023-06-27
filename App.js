85536e7ce440edc706551d688fc0cb87c291be32
Prashant Sharma <116828172+Prashantsharma3@users.noreply.github.com>
2023-06-27 13:00:12 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ const saveButton = document.querySelector("#saveButton");
 const loadSavedButton = document.querySelector("#loadSavedButton");
 const loadNewsButton = document.querySelector("#loadNewsButton");
 const categorySelect = document.querySelector("#categorySelect");
+const hiddenElement = document.querySelector("#hidden");
+const nsContentElement = document.querySelector("#nscontent");
 
 function showDiv() {
-  document.querySelector("#hidden").style.display = "block";
+  hiddenElement.style.display = "block";
 }
 
-const savedNews = [];
+const savedNewsString = localStorage.getItem("savedNews");
+const savedNews = savedNewsString ? JSON.parse(savedNewsString) : [];
 
 const handleSavedNews = (savedItem) => {
   savedNews.push(savedItem);
@@ -29,20 +32,18 @@ const getNews = (category = "science") => {
           <p>By <strong>${newsItem.author}</strong></p>
           <h2>${newsItem.title}</h2>
           <div id="box">
-          <img src="${newsItem.imageUrl}" class="img"></img>
-          <div id="innerbox">
-          <p id="nscontent">${newsItem.content} <a href="${newsItem.readMoreUrl}" style="text-decoration:none">READ MORE</a></p>
-          <p>Date:- ${newsItem.date}</p>
-          <p>Time:- ${newsItem.time}</p>
+            <img src="${newsItem.imageUrl}" class="img"></img>
+            <div id="innerbox">
+              <p id="nscontent">${newsItem.content} <a href="${newsItem.readMoreUrl}" style="text-decoration:none">READ MORE</a></p>
+              <p>Date: ${newsItem.date}</p>
+              <p>Time: ${newsItem.time}</p>
+            </div>
           </div>
-          </div>
-       
         `;
         const button = document.createElement("button");
         button.innerHTML = "Save";
-        button.onclick = function () {
-          handleSavedNews(newsItem);
-        };
+        button.classList.add("saveButton");
+        button.dataset.newsItem = JSON.stringify(newsItem);
         div.appendChild(button);
         newsContainer.appendChild(div);
       });
@@ -50,33 +51,25 @@ const getNews = (category = "science") => {
 };
 
 const saveNews = () => {
-  const news = Array.from(document.querySelectorAll(".newsItem")).map(
-    (newsItem) => {
-      return {
-        title: newsItem.querySelector("h2").textContent,
-        content: newsItem.querySelector("#nscontent").textContent
-      };
-    }
-  );
-  console.log("saved news", news);
+  const news = Array.from(document.querySelectorAll(".newsItem")).map((newsItem) => {
+    return {
+      title: newsItem.querySelector("h2").textContent,
+      content: newsItem.querySelector("#nscontent").textContent,
+    };
+  });
+
   localStorage.setItem("savedNews", JSON.stringify(news));
 };
 
 const loadSavedNews = () => {
-  console.log("Saved News", savedNews);
   newsContainer.innerHTML = "";
-  //const savedNews = JSON.parse(localStorage.getItem("savedNews"));
-  //console.log("saved news from storage", savedNews)
-  if (!savedNews) {
-    return;
-  }
+
   savedNews.forEach((newsItem) => {
     const div = document.createElement("div");
     div.classList.add("newsItem");
     div.innerHTML = `
-    <h2>${newsItem.title}</h2>
-    <p>${newsItem.content}</p>
-    
+      <h2>${newsItem.title}</h2>
+      <p>${newsItem.content}</p>
     `;
     newsContainer.appendChild(div);
   });
